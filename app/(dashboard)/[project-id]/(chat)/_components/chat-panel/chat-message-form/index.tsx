4b45b379910848049b8291270d7e772404c9bfcd
pbabula <pbabula@hprntl.ai>
2026-02-04 13:02:58 +0100

    feat(frontend): hide summarizer

--- app/(dashboard)/[project-id]/(chat)/_components/chat-panel/chat-message-form/index.tsx
+++ app/(dashboard)/[project-id]/(chat)/_components/chat-panel/chat-message-form/index.tsx
@@ -77,7 +77,8 @@ const ChatMessageForm = ({ className, ref }: ChatMessageFormProps) => {
 
     return (
         <div ref={ref} className={className}>
-            <NextStagePill />
+            {/* TODO: Add back when summarizer is implemented */}
+            {/* <NextStagePill /> */}
 
             <AnimatePresence>
                 <form onSubmit={handleSubmit(onFormSubmit)} className="relative flex items-end justify-center px-4">
