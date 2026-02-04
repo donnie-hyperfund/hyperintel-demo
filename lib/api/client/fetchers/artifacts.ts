fb1d52bc1cec9474153228aad1fb5725a4dce9a4
BartlomiejLipiec <blipiec@hprntl.ai>
2026-02-04 12:04:49 +0100

    fix(fe): timeout fix

--- lib/api/client/fetchers/artifacts.ts
+++ lib/api/client/fetchers/artifacts.ts
@@ -48,6 +48,8 @@ export function createArtifactApi(getToken: TokenGetter) {
         approveVersion: async (projectId: string, artifactId: string, versionId: string) => {
             const { data } = await axios.post<{ success: true; version: number; status: 'approved' }>(
                 ENDPOINTS.approve(projectId, artifactId, versionId),
+                undefined,
+                { timeout: 60000 },
             );
             return data;
         },
@@ -56,6 +58,7 @@ export function createArtifactApi(getToken: TokenGetter) {
             const { data } = await axios.post<{ success: true; version: number; status: 'rejected' }>(
                 ENDPOINTS.reject(projectId, artifactId, versionId),
                 { reason },
+                { timeout: 60000 },
             );
             return data;
         },
